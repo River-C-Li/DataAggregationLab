4f074bde12415e9b456ef2ec1ce51b6146f3c189
River-C-Li <chengrui8848@gmail.com>
2024-11-28 16:27:33 -0500

    Update grades._stats.mjs

--- routes/grades._stats.mjs
+++ routes/grades._stats.mjs
@@ -24,29 +24,47 @@ router.get("/grades/stats", async (req, res) => {
     }
 }
 ]);
+return result
 });
 
 // Create a GET route at /grades/stats/:id
 router.get("/grades/stats/:id", async (req, res) => {
   let collection = await db.collection("grades");
-  let result = await collection
-    .aggregate([
-
-
-    ])
-  })
 // Within this route, mimic the above aggregation pipeline, but only for learners within a class that has a class_id equal to the specified :id.
 // Create a single-field index on class_id.
-
+collection.createIndex({ class_id: 1}, (err, result));
 // Create a single-field index on learner_id.
-
+collection.createIndex({ learner_id: 1}, (err, result));
 // Create a compound index on learner_id and class_id, in that order, both ascending.
+collection.createIndex({ learner_id: 1,  class_id: 1}, (err, result));
 
 // Create the following validation rules on the grades collection:
-
-
 // Each document must have a class_id field, which must be an integer between 0 and 300, inclusive.
-
+async() =>{
+  await db.createCollection("grades",
+    {
+    validator:{
+      $jsonSchema:{
+      bsonType: "object",
+      required: ["class_id", "learner_id"],
+      properties: {
+        class_id:{
+          bsonType: "int",
+          minimum: 0,
+          maxmum: 300,
+          description: "class_id is required and must be an integer between 0 and 300, inclusive."
+        },
 // Each document must have a learner_id field, which must be an integer greater than or equal to 0.
-
+        learner_id:{
+          bsonType: "int",
+          minimum: 0,
+          description: "learner_id is required and must be an integer greater than or equal to 0."
+        }
+      }
+      }
+    }
+  })
+}
 // Change the validation action to "warn."
+})
+
